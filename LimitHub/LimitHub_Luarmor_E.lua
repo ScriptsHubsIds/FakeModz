7d1593cc4fc2607dac8f063be86c031f3e5da2ff
Lib <ybanezjosevincent.bshm.cpc@gmail.com>
2025-07-26 20:07:36 +0800

    Update LimitHub_Luarmor_E.lua

--- LimitHub/LimitHub_Luarmor_E.lua
+++ LimitHub/LimitHub_Luarmor_E.lua
@@ -5,7 +5,7 @@ loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/vCqomMAneDVdhHL4/raw", t
 end
 local function loadui()
 
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
 
 end
 
